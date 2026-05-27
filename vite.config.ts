4e7e582d4557c16dc17c48819ac938fffb9df602
nbmprod <nikitabychkov96@gmail.com>
2026-05-27 20:46:47 +0200

    clean up

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Resolve entry from project root
   root: '.',
+  
+  // Base public path when served in production
+  // Using './' ensures asset paths are resolved relative to index.html
+  base: './',
 
   build: {
     target: 'es2022',
